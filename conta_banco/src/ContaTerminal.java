5192f0703543dabfdeeba1ca168443c820e5a378
Talita Rienzi <talitarp134@gmail.com>
2023-06-06 21:45:18 -0300

    Criação do código com a lógica do sistema

--- conta_banco/src/ContaTerminal.java
+++ conta_banco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
-public class App {
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo = 237.48;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("                Sistema Bancário");
+        System.out.println("                -*-*-*-*-*-*-*-*-");
+
+        System.out.println("Digite o seu nome completo! ");
+        nomeCliente = teclado.nextLine();
+
+        System.out.println("Por favor, digite o número da agencia! ");
+        agencia = teclado.nextLine();
+        
+        System.out.println("Por favor, digite o número da conta! ");
+        numeroConta = teclado.nextInt();
+
+        
+        System.out.println("Olá "+ nomeCliente + ", obrigada por criar uma conta em nosso banco, sua agência é "
+                            +agencia+ " conta " + numeroConta + " e seu saldo " + saldo + " já está disponivel para saque." );
+
+        teclado.close();
     }
+
+   
 }
+
